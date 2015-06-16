943a66cbbf752bb2b1233c4ac7a17cbc6f418b89
amitpawar <sunny538889@hotmail.com>
2015-06-17 00:00:20 +0200

    Test travis

--- thesis/src/test/java/TestCross.java
+++ thesis/src/test/java/TestCross.java
@@ -35,7 +35,7 @@ public class TestCross {
 
         crossSet.print();
 
-        TupleGenerator tupleGenerator = new TupleGenerator(env,4);
+        TupleGenerator tupleGenerator = new TupleGenerator(env,3);
 
         for(SingleOperator operator : tupleGenerator.getOperatorTree()){
             TestCase.assertNotNull(operator.getOperatorOutputAsList());
