333142b52da0b85ae49ca6feb798327c9967d37e
amitpawar <sunny538889@hotmail.com>
2015-06-17 01:09:36 +0200

    Test travis.....

---
+++ flink-illustrator/src/test/java/Config.java
@@ -0,0 +1,38 @@
+
+
+public class Config {
+
+
+	public static String pathToVisits() {
+		return "src/test/resources/visits";
+	}
+
+	public static String pathToVisitsEU() {
+		return "src/test/resources/visitseu";
+	}
+
+	public static String pathToVisitsHDFS(){return "hdfs://localhost:9000/thesis/visits";}
+
+
+
+	public static String pathToUrls() {
+		return "src/test/resources/urls";
+	}
+
+	public static String pathToCoordSet1() {
+		return "/home/amit/thesis/dataflow/coordSet1";
+	}
+
+	public static String pathToCoordSet2() {
+		return "/home/amit/thesis/dataflow/coordSet2";
+	}
+
+	public static String pathToSet1(){return "src/test/resources/CrossInput1";}
+
+    public static String pathToSet2(){return "src/test/resources/CrossInput2";}
+
+
+	public static String outputPath() {
+		return "src/resources/output";
+	}
+}
